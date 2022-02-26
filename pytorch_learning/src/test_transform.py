82475ac7aa1438adba0660e5f906a4459e47f2dd
SZ001S <sz001s@qq.com>
2022-02-26 21:02:58 +0800

    新文件
    Signed-off-by: SZ001S <sz001s@qq.com>

--- pytorch_learning/src/test_transform.py
+++ pytorch_learning/src/test_transform.py
@@ -1,4 +1,20 @@
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
+from PIL import Image
 # transforms.py是一个工具箱 能将比如图片 通过totensor，resize等工具 产生一个结果
+# python -> tensor数据类型
+# transforms.ToTensor()创建具体的工具
 
-# 
+
+img_path = '../datasets/train/ants_image/0013035.jpg'
+img = Image.open(img_path)
+
+writer = SummaryWriter('logs')
+
+tensor_trains = transforms.ToTensor()
+tensor_img = tensor_trains(img)
+# print(tensor_img)
+
+writer.add_image('Tensor_img', tensor_img)
+
+writer.close()
