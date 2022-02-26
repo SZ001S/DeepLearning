7b2bcb3b0c974babf3d6eb75d360c493ec9c578e
SZ001S <sz001s@qq.com>
2022-02-26 22:09:27 +0800

    新文件
    Signed-off-by: SZ001S <sz001s@qq.com>

--- pytorch_learning/src/usefulTransforms.py
+++ pytorch_learning/src/usefulTransforms.py
@@ -18,5 +18,18 @@ img_norm = trans_norm(img_tensor)
 print(img_norm[0][0][0])
 writer.add_image('Normalize', img_norm, 2)
 
+# Resize
+print(img.size)
+trans_resize = transforms.Resize((512, 512))
+img_resize = trans_resize(img)
+img_resize = trans_totensor(img_resize)
+writer.add_image("Resize", img_resize, 0)
+print(img_resize)
+
+# Compose - resize - 2 能将好几个transforms的操作进行打包
+trans_resize_2 = transforms.Resize(512)
+trans_compose = transforms.Compose([trans_resize_2, trans_totensor])
+img_resize_2 = trans_compose(img)
+writer.add_image("Resize", img_resize_2, 1)
 writer.close()
 
